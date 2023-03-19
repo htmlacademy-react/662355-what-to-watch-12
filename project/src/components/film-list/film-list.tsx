5c5fe43119718cf3ba92fcc0cb73bfb19e6cf2fd
Evgeniia Dmitrieva <be230497@gmail.com>
2023-03-19 19:51:25 +0700

    удалила не использованный параметр

--- project/src/components/film-list/film-list.tsx
+++ project/src/components/film-list/film-list.tsx
@@ -5,7 +5,7 @@ import FilmCard from '../film/film';
 type FilmListProps = { films: Films };
 
 export function FilmList({ films }: FilmListProps): JSX.Element {
-  const [filmId, setFilmId] = useState(-1);
+  const [, setFilmId] = useState(-1);
   return (
     <div className="catalog__films-list">
       {films.map((film) => <FilmCard film={film} key={film.id} onMouseOver={() => { setFilmId(film.id); }} />)}
